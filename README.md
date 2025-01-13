be24508d4e5266353cb5753e3254f4efaa3a6b6b
Rostyslav Shynlov <shynlov@fuib.com>
2025-01-13 19:05:02 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SUS JW SERVER
---
+++ src/test/java/com/lirugo/sus_jw/SusJwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lirugo.sus_jw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusJwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
