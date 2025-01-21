d73427a470faf9c7f27c0ab1e5c8850a006956ab
Rostyslav Shynlov <shynlov@fuib.com>
2025-01-21 20:45:31 +0200

    feat(mvp): Get stand-day by id

--- src/main/java/com/lirugo/sus_jw/controller/StandDayController.java
+++ src/main/java/com/lirugo/sus_jw/controller/StandDayController.java
@@ -4,10 +4,14 @@ import com.lirugo.sus_jw.dto.StandDayDto;
 import com.lirugo.sus_jw.service.StandDayService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/stand-days")
@@ -18,5 +22,12 @@ public class StandDayController {
     @GetMapping
     public List<StandDayDto> getStands() {
         return standDayService.getStandDays();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StandDayDto> getStand(@PathVariable long id) {
+        return standDayService.getById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/lirugo/sus_jw/service/StandDayService.java
+++ src/main/java/com/lirugo/sus_jw/service/StandDayService.java
@@ -4,6 +4,7 @@ import com.lirugo.sus_jw.dto.StandDayDto;
 import com.lirugo.sus_jw.mapper.StandDayMapper;
 import com.lirugo.sus_jw.repo.StandDayRepo;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,10 @@ import org.springframework.stereotype.Service;
 public class StandDayService {
     private final StandDayRepo standDayRepo;
     private final StandDayMapper standDayMapper;
+
+    public Optional<StandDayDto> getById(long id) {
+        return standDayRepo.findById(id).map(standDayMapper::map);
+    }
 
     public List<StandDayDto> getStandDays() {
         return standDayRepo.findAll().stream().map(standDayMapper::map).toList();
