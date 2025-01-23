c821b563424da3af914e00d1469cfe01e6ccfe2a
Rostyslav Shynlov <shynlov@fuib.com>
2025-01-23 12:01:14 +0200

    feat(mvp): Seed stand days 01.25

---
+++ src/main/resources/db/migration/V1_2__seed_stands.sql
@@ -0,0 +1,8 @@
+insert into stands (id, key, name, description, latitude, longitude) values
+    (1, 'mkuw-metro', 'Метро (Троїцький)', 'Стенд біля метро академмістечко зі сторони троїцького ринку', null, null),
+    (2, 'mkuw-novus', 'Новус (Макдональдс)', 'Стенд біля метро академмістечко зі сторони Novus та McDonald`s', null, null),
+    (3, 'mkuw-byzova', 'Бузова', 'Стенд в Бузовій', null, null),
+    (4, 'mkuw-dachna-varus', 'Дачна (Варус)', 'Стенд біля автостанції дачна зі сторони магазину Варус', null, null),
+    (5, 'mkuw-dachna-park', 'Дачна (Парк)', 'Стенд біля автостанції дачна в середині парку', null, null),
+    (6, 'mkuw-chaika', 'Чайка (Парус)', 'Стенд на Чайці', null, null),
+    (7, 'mkuw-velmart', 'Велмарт', 'Стенд біля магазину Велмарт', null, null);
